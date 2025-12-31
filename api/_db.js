47c2d28b197632733d354d4d8d6e96fb79cdcc07
Carlos Pena <carlos.pena2@endava.com>
2025-12-31 01:05:52 -0500

    Use explicit Postgres params with SSL

--- api/_db.js
+++ api/_db.js
@@ -5,7 +5,11 @@ let pool
 const getPool = () => {
   if (!pool) {
     pool = new Pool({
-      connectionString: process.env.POSTGRES_URL_NON_POOLING,
+      host: process.env.POSTGRES_HOST,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      port: 5432,
       ssl: { rejectUnauthorized: false }
     })
   }
