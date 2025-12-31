aae7fd2856214665fb8d44a1ecbcaae98d122f8a
Carlos Pena <carlos.pena2@endava.com>
2025-12-31 00:54:59 -0500

    Fix Postgres SSL for Vercel

--- api/_db.js
+++ api/_db.js
@@ -5,7 +5,8 @@ let pool
 const getPool = () => {
   if (!pool) {
     pool = new Pool({
-      connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL
+      connectionString: process.env.POSTGRES_URL || process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     })
   }
   return pool
